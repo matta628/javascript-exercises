5554c7e852e771003104c420d3424b798f128bd3
Matt <matta628@gmail.com>
2022-11-09 09:47:36 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    if ((typeof low != 'number') || (typeof high != 'number') || low < 0 || high < 0){
+        return 'ERROR';
+    }
 
+    if (low > high){
+        let temp = low;
+        low = high;
+        high = temp;
+    }
+
+    let sum = 0;
+    for (let i = low; i <= high; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
